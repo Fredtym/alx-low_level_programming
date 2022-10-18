779be577be60ad0ead08fc116b9c6bff4a72fac7
Fredtym <niiayitey123@gmail.com>
2022-10-18 22:19:03 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -calculates the sum of two integers
+ * @number1: first number
+ * @number2: second number
+ * Return: sum of the numbers
+ */
+int add(int number1, int number2)
+{
+	int res;
+
+	res = num1 + num2;
+	return (res);
+}
