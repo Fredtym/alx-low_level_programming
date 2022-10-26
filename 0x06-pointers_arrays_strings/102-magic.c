c8e93b9bcf519e14cf1a34572dd3a83b3374f06e
Fredtym <niiayitey123@gmail.com>
2022-10-26 23:41:35 +0000

    task 15

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+/** 
+*main:print_line - prints a s bytes of a buffer
+
+*/
+
 int main(void)
-{
+{	/**
+ * main:print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
 	int n;
 	int a[5];
 	int *p;
