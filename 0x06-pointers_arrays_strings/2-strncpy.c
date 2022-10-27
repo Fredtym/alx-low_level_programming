a3b4c7df477c36d82b4f3b836b1fd03b2d6cf4d3
Fredtym <niiayitey123@gmail.com>
2022-10-27 00:09:55 +0000

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
 /**
  * _strncpy - Copies at most an inputted number
  * of bytes from string src into dest.
- * @dest: The variable storing the string copy.
+ * @dest: The bugger storing the string copy.
  * @src: The source string.
  * @n: The source string.
  *
@@ -23,3 +23,4 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[index] = '\0';
 
 	return (dest);
+}
